f60900c2694725280c329c64577d90b601af47c2
Aurélien PELISSIER <aurelien.pelissier.38@gmail.com>
2022-09-16 16:01:46 +0200

    updated plot

--- Analyze_data.py
+++ Analyze_data.py
@@ -19,7 +19,8 @@ questions += ['1_5'] # current number of members
 questions += ['1_8'] # age group of the majority of today's members?
 questions += ['1_9_1'] # Is membership in your cooperative linked to any special conditions
 #questions += [('2_3_%s_1' % i) for i in range(1,17)] # In which areas is your cooperative currently active
-questions += ['3_2'] #type of system
+questions += ['3_2_1_1'] #Is_solar
+questions += ['3_2_1_3'] #Solar power (kW)
 questions += ['3_5'] #How did you sell the generated electricity and ecological added value
 questions += ['5_1'] #starting capital
 questions += ['5_10_1'] #more than 50% owned by one person
@@ -30,11 +31,13 @@ question_label += ['Current number of members']
 question_label += ["Age group of the majority of today's members"]
 question_label += ["Is membership in your cooperative linked to any special conditions"]
 #question_label += []
-question_label += ['Type of system']
+question_label += ['Is solar ?']
+question_label += ['Solar power (kW)']
 question_label += ['How did you sell the generated electricity and ecological added value']
 question_label += ['Starting capital [CHF]']
 question_label += ['More than 50% owned by one person ?']
 question_label += ['Purpose of cooperative']
+#question_label += []
 
 print(questions)
 
@@ -44,14 +47,14 @@ for label in df.columns:
     if QQ in questions:
         index = np.where(np.array(questions) == QQ)[0][0]
         data = df[[label]].to_numpy().reshape(-1)
-        data = np.array([x for x in data if x!=' '])
+        data = np.array([str(x).replace(',','.') for x in data if x!=' '])
         try:
-            data = data.astype(int)
+            data = data.astype(float)
         except:
             pass
 
         plt.figure(figsize=(8,4))
-        if label == '@1_5' or label == '@5_1':
+        if label in ['@1_5','@5_1','@3_2_1_3']:
             data[data==0]=1
             plt.hist(np.log(data), rwidth=0.9, color='black')
             labels_ = set(data)
